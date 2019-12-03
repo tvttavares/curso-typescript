91c2b5cc05d50cd348786b0afccaf80797037e7e
thiago <tvttavares@gmail.com>
2019-12-03 19:51:39 -0400

    Fundamentos do TypeScript

---
+++ tipos-basicos/helloworld.js
@@ -0,0 +1,6 @@
+// String nome
+// nome: string
+function dizerOla(nome) {
+    console.log("Ola " + nome);
+}
+dizerOla("Algaworks");
---
+++ tipos-basicos/helloworld.ts
@@ -0,0 +1,7 @@
+// String nome
+// nome: string
+function dizerOla(nome: String) {
+    console.log(`Ola ${nome}`);
+}
+
+dizerOla("Algaworks");
