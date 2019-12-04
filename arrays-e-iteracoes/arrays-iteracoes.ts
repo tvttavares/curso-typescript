f5b66e9ff3eabe4c0554bdcc69ef438c953023af
thiago <tvttavares@gmail.com>
2019-12-03 20:45:23 -0400

    Arrays e iterações

---
+++ arrays-e-iteracoes/arrays-iteracoes.ts
@@ -0,0 +1,11 @@
+let notasAlunos: number[] = [10, 8, 8.5, 7];
+
+// indices - in
+for (let i in notasAlunos) {
+    console.log(i);
+}
+
+// valores - of
+for (let nta of notasAlunos) {
+    console.log(nta);
+}
